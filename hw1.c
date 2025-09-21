9bca0f0c9fbe6001c2a460282d46ef4c17511544
gorDaChris <gorgo@MSI.localdomain>
2025-09-21 14:48:37 -0400

    Added cmd arguments and README

--- hw1.c
+++ hw1.c
@@ -1,6 +1,16 @@
 #include "../unpv13e-master/lib/unp.h" // change according to directory
 #include <stdio.h> // support I/O
+#include <stdlib.h> // for atoi
 
 int main(int argc, char **argv) {
+	if (argc != 3) { // outfile + two arguments
+		perror("ERROR: Two arguments allowed only");
+		exit(EXIT_FAILURE);
+	}
+
+	int lowestPort = atoi(argv[1]); // used instead of 69
+	int highestPort = atoi(argv[2]);
+
+
 	return 0;
 }
